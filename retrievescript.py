b16cdb36e2f2df7329e286a002b5fe9531f5c122
Leo <lpguercio@gmail.com>
2020-07-26 15:56:41 +0200

    MINOR UPDATE Improving formatting in retrievescript.py

--- retrievescript.py
+++ retrievescript.py
@@ -6,20 +6,29 @@ from dataretriever import DataRetriever
 
 parser = argparse.ArgumentParser(description="Retrieves binance futures' data")
 
-today = datetime.datetime.combine(datetime.date.today(), datetime.time.fromisoformat('00:00'))
-later = datetime.datetime.combine(today, datetime.time.fromisoformat('08:00'))
+today = datetime.datetime.combine(datetime.date.today(),
+                                  datetime.time.fromisoformat('00:00'))
+later = datetime.datetime.combine(today,
+                                  datetime.time.fromisoformat('08:00'))
 
 # Adding the flags to be parsed by the script
 parser.add_argument('--pair',
                     help='The pair for which to retrieve data', default='BTCUSDT')
+
 parser.add_argument('--start',
-                    help='Date or time for the starting point of data gathering. Should be in iso format',
+                    help='Date or time for the starting point of data gathering. \
+                         Should be in iso format',
                     default=today.isoformat())
+
 parser.add_argument('--end',
-                    help='Date or time for the ending point of data gathering. Should be in iso format',
+                    help='Date or time for the ending point of data gathering. \
+                         Should be in iso format',
                     default=later.isoformat())
+
 parser.add_argument('--timeframe',
-                    help='The timeframe for the candles: 1m or 3m or 5m...', default='1m')
+                    help='The timeframe for the candles: 1m or 3m or 5m...',
+                    default='1m')
+
 parser.add_argument('--limit', help='Limit of candles to retrieve', default=1500)
 
 args = parser.parse_args()
